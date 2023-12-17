c8b16ffe06d639c5eb7c09acc411398223a430cf
Keith <shadow902@gmail.com>
2023-12-16 23:56:00 -0500

    add homepage carousel 1

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,60 @@ import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
+
 <template>
-  <RouterView />
+  <v-app class="rounded rounded-md">
+    <!-- <v-app-bar title="Application bar"> -->
+      <v-toolbar density="compact">
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+        <v-toolbar-title>
+          <RouterLink to="/">Annual Raffle</RouterLink>
+        </v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-btn
+          to="/buytickets"
+          variant="outlined"
+          size="small"
+        >
+          Buy Tickets
+        </v-btn>
+
+        <v-btn
+          to="/learnmore"
+          variant="outlined"
+          size="small"
+        >
+          Learn More
+        </v-btn>
+
+        <!-- <v-btn icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+
+        <v-btn icon>
+          <v-icon>mdi-heart</v-icon>
+        </v-btn>
+
+        <v-btn icon>
+          <v-icon>mdi-dots-vertical</v-icon>
+        </v-btn> -->
+      </v-toolbar>
+
+    <!-- </v-app-bar> -->
+
+    <!-- <v-navigation-drawer>
+      <v-list>
+        <v-list-item title="Navigation drawer"></v-list-item>
+      </v-list>
+    </v-navigation-drawer> -->
+
+    <v-main style="min-height: 300px;">
+      <!-- Main Content -->
+      <RouterView />
+    </v-main>
+  </v-app>
 </template>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,48 +1,77 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+<template>
+  <v-carousel 
+    :show-arrows="false"
+    cycle
+    height="400"
+    hide-delimiter-background
+    show-arrows="hover"
+  >
+    <v-carousel-item
+      v-for="(item,i) in items"
+      :key="i"
+      :src="item.src"
+      cover
+    >     
+      <div class="d-flex fill-height justify-center align-center">
+        <div class="text-h2 text-white" >
+          {{ item.slides }} Prize: {{ item.prizes }}          
+        </div>
+      </div>
+    </v-carousel-item>
+</v-carousel>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        items: [
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+            slides: 'First',
+            prizes: '$2000',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+            slides: 'Second',
+            prizes: '$1000',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+            slides: 'Third',
+            prizes: '$500',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+            slides: 'Fourth',
+            prizes: '50" Smart TV',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+            slides: 'Fifth',
+            prizes: '$100',
+          },
+        ],
+        slides: [
+          'First',
+          'Second',
+          'Third',
+          'Fourth',
+          'Fifth',
+        ],
+        prizes: [
+          '$2000',
+          '$1500',
+          '$1000',
+          '50 in nFlat-screen TV',
+          '$100',
+        ],
+      }
+    },
+  }
 </script>
 
-<template>
-  <!-- <v-card
-    color="grey-darken-4"
-    flat
-    height="200px"
-    rounded="0"
-  > -->
-    <v-toolbar density="compact">
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
-      <v-toolbar-title>Annual Raffle</v-toolbar-title>
 
-      <v-spacer></v-spacer>
 
-      <v-btn
-        to="/buytickets"
-        variant="outlined"
-        size="small"
-      >
-        Buy Tickets
-      </v-btn>
 
-      <v-btn
-        to="/learnmore"
-        variant="outlined"
-        size="small"
-      >
-        Learn More
-      </v-btn>
-
-      <!-- <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-dots-vertical</v-icon>
-      </v-btn> -->
-    </v-toolbar>
-  <!-- </v-card> -->
-</template>
